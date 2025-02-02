cb03efb2da9e6fa680ed3027288ee79ff7a0d094
mjimenez <mjimenez@dexfreight.io>
2025-02-02 01:15:00 -0500

    Fix: * optimization for vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  Sitemap({ hostname: "https://madnesslab.dev" }),
+  ],
   build: {
     minify: 'esbuild', // Faster minification
     chunkSizeWarningLimit: 500, // Adjust to avoid warnings
